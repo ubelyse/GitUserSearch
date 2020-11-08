7b0fd057a1ae3861ada4da2635e3734b8ebdbcbc
Belyse Uwambayinema <ubelyse1@gmail.com>
2020-11-08 10:37:57 +0200

    added the repo

--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -13,7 +13,7 @@ export class ProfileService {
 
   constructor(private http:HttpClient) {
     
-    this.userclass=new Userclass('','','',0,0,0,new Date())
+    this.userclass=new Userclass('','',0,0,0,'',new Date())
     this.repo= []
    }
    profileRequest(userInput){
@@ -23,21 +23,21 @@ export class ProfileService {
     interface ApiResponse{
       name:string;
       avatar_url:string;
-      location:string;
       followers:number;
       following:number;
       repo:number;
+      repo_url:string;
     }
 
     let promise =new Promise((resolve,reject)=>{
       this.http.get<ApiResponse>('https://api.github.com/users/' + username+'?access_token='+ environment.apikey).toPromise().then(response=>{
           
           this.userclass.username=response.name
-          this.userclass.location=response.location
           this.userclass.avatar_url=response.avatar_url
           this.userclass.followers=response.followers
           this.userclass.following=response.following
           this.userclass.repo=response.repo
+          this.userclass.repo_url=response.repo_url
          
         
           resolve()
